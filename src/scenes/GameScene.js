b106dfa40ac037fdebb72dfab937a761247df374
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-11-15 13:46:09 -0800

    got the paltforms to render

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -7,16 +7,26 @@ export default class GameScene extends Phaser.Scene {
 
   preload() {
     this.load.image('sky', 'assets/sky.png')
-    this.load.image('ground', '../../assets/ground.png')
-    this.load.image('platform', '../../assets/platform.png')
-    this.load.image('bomb', '../../assets/bomb.png')
+    // this.load.image('ground', 'assets/ground.png')
+    this.load.image('platform', 'assets/platform.png')
+    this.load.image('bomb', 'assets/bomb.png')
 
-    this.load.spritesheet('onion', '../../assets/onion.png', {
+    this.load.spritesheet('onion', 'assets/onion.png', {
       frameWidth: 32 , frameHeight: 48
     })
   }
   create() {
     this.add.image(400, 300, 'sky')
+    this.createPlatforms()
+    
   }
+  createPlatforms() {
+    const platforms = this.physics.add.staticGroup()
+    platforms.create(400, 568, 'platform').setScale(2).refreshBody()
+    platforms.create(600, 400, 'platform') 
+    platforms.create(50, 250, 'platform') 
+    platforms.create(750, 220, 'platform') 
+  
+}
 }
 
